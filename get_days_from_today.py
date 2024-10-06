f49a76b27f4bf45910d4bdd6764e852e639d968f
Vitalii Poliakov <95705028+vitalik211287@users.noreply.github.com>
2024-10-06 16:38:47 +0300

    get_numbers_ticket

--- get_days_from_today.py
+++ get_days_from_today.py
@@ -6,15 +6,22 @@ date = "2021-05-05"
 
 def get_days_from_today(date: str) -> int:
     try:
+        # Спроба перетворити рядок на дату у форматі "YYYY-MM-DD"
         given_date = datetime.strptime(date, "%Y-%m-%d")
-        current_date = datetime.strptime(str(datetime.now().date()), "%Y-%m-%d")  
+        current_date = datetime.strptime(str(datetime.now().date()), "%Y-%m-%d") 
+       
+        # Обчислення різниці у днях
         days_difference = current_date.toordinal() - given_date.toordinal()
         return days_difference
     except ValueError:
+       
+        # Обробка помилки неправильного формату дати
         return "Помилка: Невірний формат дати! Формат повинен бути YYYY-MM-DD."
 
 
 print(get_days_from_today(date)) 
+
+
 
 
 
---
+++ get_numbers_ticket.py
@@ -0,0 +1,28 @@
+import random
+
+
+def get_numbers_ticket(min: int, max: int, quantity: int) -> int:
+    
+        
+    try:
+        # Перевірка чи всі аргументи є цілими числами
+        if min != int(min) or  max != int(max) or quantity != int(quantity) :
+            return TypeError("Усі вхідні дані повинні бути цілими числами.")
+        
+        # Перевірка, чи максимальне значення більше мінімального
+        if min > max:
+            return  ValueError("Мінімальне значення не може бути більше за максимальне.")
+        
+        # Створення списку від мінімального до максимального числа
+        list_numbers = list(range(min, max + 1))
+        
+        # Вибір випадкових чисел
+        win_numbers = random.sample(list_numbers, k=quantity )
+        return win_numbers
+    except TypeError:  "Помилка: Невірний формат даних! " 
+    
+    
+
+print(get_numbers_ticket(1, 1000, 6))
+
+
